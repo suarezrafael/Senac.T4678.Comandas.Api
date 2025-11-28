cb1ac47d3096f79c7925c8f2879aff775486c13f
Rafael Vieira Suarez <rafaelv_s@hotmail.com>
2025-11-27 21:32:33 -0300

    removido comentario do Program.cs

--- Comandas.Api/Comandas.Api/Program.cs
+++ Comandas.Api/Comandas.Api/Program.cs
@@ -4,24 +4,18 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//Configurar o contexto do banco de dados para usar o InMemoryDatabase
 builder.Services.AddDbContext<ComandasDbContext>(options =>
     options.UseSqlite("DataSource=comandas.db")
 );
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
 app.UseSwagger();
 app.UseSwaggerUI();
-//}
 
 app.UseHttpsRedirection();
 
